30fc8cedb4401a59444c38fb7ef50715b1ca19cb
Fermín Bianco <ferminbianco40@gmail.com>
2023-06-09 14:19:01 -0300

    ejes y partials

--- app.js
+++ app.js
@@ -5,29 +5,32 @@ const path = require("path")
 const publicPath = path.resolve(__dirname, "./public")
 app.use(express.static(publicPath))
 
+app.set ("view engine", "ejs")
+app.set ("views", path.join (__dirname,"./views"))
+
 app.listen(3010, ()=> {
     console.log("Jarvis Iniciado en puerto 3010")
 })
 
 app.get("/", (req, res)=>{
-    res.sendFile(path.join(__dirname,"/views/home.html"))
+    res.render("home")
 })
 
 app.get("/Registro", (req, res)=>{
-    res.sendFile(path.join(__dirname,"/views/formRegistro.html"))
+    res.render("users/formRegistro")
 })
 
-app.get("/Login", (req, res)=>{
-    res.sendFile(path.join(__dirname,"/views/login.html"))
+app.get("/login", (req, res)=>{
+    res.render("users/login")
 })
 
 app.get("/productDetail", (req, res)=>{
-    res.sendFile(path.join(__dirname,"/views/productDetail.html"))
+    res.render("productos/productDetail")
 })
 app.get('/carrito',(req,res)=>{
-    res.sendFile(path.join(__dirname,"./views/carrito.html"))
+    res.render("carrito")
 })
 
 app.get('/creation',(req,res)=>{
-    res.sendFile(path.join(__dirname,"./views/creacionProducto.html"))
+    res.render("creacionProducto")
 })
