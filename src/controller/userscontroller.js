e7fd317323fe4af3c6265c579869c18af2391408
Nathymmd <nathycmmd2001@gmail.com>
2023-06-24 16:12:17 +0200

    edicion de usuario

--- src/controller/userscontroller.js
+++ src/controller/userscontroller.js
@@ -1,10 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+const rutaArchivo = path.resolve('./src/database/users.json');
+const userUsers = JSON.parse(fs.readFileSync(rutaArchivo));
+
+
 let registrocontroller = {
     registroUser: function (req, res) {
       res.render("../views/users/formRegistro")
     },
     loginUser:function (req,res) {
       res.render("../views/users/login")
+    },
+
+    edit: (req,res)=>{
+      const buscarUsurio = userUsers.find(row=>row.id==req.params.id)
+      if (buscarUsurio) return res.render('users/edicionUser', { user: buscarUsurio})
+              else return res.send("ERROR 404 NOT FOUND")
+    },
+  
+    editProcess:(req,res)=>{
+      let editarUsuario = {}
+      editarUsuario = userUsers.find(row => row.id == req.params.id)
+      editarUsuario.nombre = req.body.nombre
+      editarUsuario.apellido = req.body.apellido 
+      editarUsuario.celular = req.body.celular 
+      editarUsuario.correo = req.body.correo
+      editarUsuario.correo = req.body.password
+      editarUsuario.avatar = req.file.filename  
+      
+      fs.writeFileSync(rutaArchivo, JSON.stringify(userUsers, null, 2), "utf-8") 
+      //console.log(req.editarUserio)
+      return res.redirect("/")
+          
     }
+
+
+
 };
+
+
 
 module.exports = registrocontroller
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,30 @@ let express = require ("express");
 let userController = require ("../controller/userscontroller");
 let router = express.Router();
 
+let path = require('path');
+const multer = require("multer");
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '../../public/images/user'))
+    },
+    filename: function (req, file, cb) {
+      let imageName = Date.now() + path.extname(file.originalname)
+      cb(null, imageName)
+    }
+  });
+
+let uploadFile = multer({ storage: storage });
+
+
 router.get ("/registro", userController.registroUser);
 router.get ("/login", userController.loginUser);
+
+
+//Editar
+router.get('/users/edit/:id', userController.edit);
+//router.put('/users/:id', userController.editProcess);
+router.put('/users/:id', uploadFile.single('imagen'),userController.editProcess);
+
 module.exports = router;
