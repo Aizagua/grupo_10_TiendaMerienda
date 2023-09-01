c80860bc326b4863e8302ec95bda8f93dad17565
Santiago <santiagoandujar10@gmail.com>
2023-09-01 20:24:50 -0300

    middleware de blockeo por admin

--- src/middlewares/adminMiddleware.js
+++ src/middlewares/adminMiddleware.js
@@ -1,17 +1,17 @@
 adminMiddleware = module.exports = (req, res, next) => {
-    try {
+  try {
+    res.locals.isAdmin = false;
+    console.log('req.session.usuarioLogeado',req.session.usuarioLogeado )
+    console.log('req.session.admin',req.session.admin )
+    if (req.session.usuarioLogeado && req.session.usuarioLogeado.id_perfil === 1) {
+      res.locals.isAdmin = true;
+    } else {
       res.locals.isAdmin = false;
-      console.log('req.session.usuarioLogeado',req.session.usuarioLogeado )
-      console.log('req.session.admin',req.session.admin )
-      if (req.session.usuarioLogeado && req.session.usuarioLogeado.id_perfil === 1) {
-        res.locals.isAdmin = true;
-      } else {
-        res.locals.isAdmin = false;
-      }console.log(req.session.admin )
-      next();
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    }console.log(req.session.admin )
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
   
--- src/routes/products.js
+++ src/routes/products.js
@@ -5,7 +5,8 @@ let router = express.Router();
 const logMiddleware = require("../middlewares/logMiddleware");
 const validacionesProducto = require('../middlewares/validacionesProducto')
 const multerMiddleware = require ('../middlewares/multer')
-const adminMiddleware = require('../middlewares/adminMiddleware')
+const adminMiddleware = require('../middlewares/adminMiddleware');
+const adminBlockeadeMiddleware = require("../middlewares/adminBlockeadeMiddleware");
 
 //Detalle
 router.get('/detail/:id', products.detalle);
@@ -16,7 +17,7 @@ router.get('/products/create', adminMiddleware, products.add);
 router.post('/products', multerMiddleware.single('imagen'), validacionesProducto, products.processCreate)
 
 //Editar Producto
-router.get('/products/edit/:id', adminMiddleware,products.edit)
+router.get('/products/edit/:id', adminMiddleware, adminBlockeadeMiddleware,products.edit)
 router.get('/products/edit', adminMiddleware,products.mostrarProducto)
 router.put('/products/:id', multerMiddleware.single('imagen'), validacionesProducto, products.editProcess)
 
