581dde2714e37a74da768256fb928eb56b1662c7
Nathymmd <nathycmmd2001@gmail.com>
2023-09-13 17:28:38 +0200

    grabar la imagen en prodcuto y ver listado de productos all users

--- src/routes/products.js
+++ src/routes/products.js
@@ -17,14 +17,14 @@ router.get('/products/create', adminBlockeadeMiddleware, products.add);
 router.post('/products', multerMiddleware.single('imagen'), validacionesProducto, products.processCreate)
 
 //Editar Producto
-router.get('/products/edit/:id', adminBlockeadeMiddleware, adminBlockeadeMiddleware,products.edit)
+router.get('/products/edit/:id', adminBlockeadeMiddleware, products.edit)
 router.get('/products/edit', adminBlockeadeMiddleware,products.mostrarProducto)
-router.put('/products/:id', multerMiddleware.single('imagen'), validacionesProducto, products.editProcess)
+router.put('/products/:id', adminBlockeadeMiddleware, multerMiddleware.single('imagen'), validacionesProducto, products.editProcess)
 
 //Borrar Producto
 router.get('/products/delete/:id',logMiddleware,products.delete)
 router.delete('/products/:id',adminBlockeadeMiddleware, products.deleteProcess)
 
-router.get ("/productList",adminBlockeadeMiddleware, products.list);
+router.get ("/productList", products.list);
 
 module.exports = router;
