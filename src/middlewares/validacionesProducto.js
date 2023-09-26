660b0c2f7677b8436e0668bed29e17e74b513f9f
Leomartin <leonelmartin9808>
2023-09-26 15:14:08 -0300

    .

--- src/middlewares/validacionesProducto.js
+++ src/middlewares/validacionesProducto.js
@@ -5,14 +5,14 @@ module.exports = [
     check('precio').isInt({gt:0}).withMessage('Indicá el precio'),
     check('cantidad').isFloat({min: 1, max: 100}).withMessage('Indica una cantidad menor a 100'),
     check('codigo').notEmpty().withMessage('Indicá el codigo'),
-    check('descripcion').isLength({min: 10, max: 50}).withMessage("Debe contener de 10 a 50 caracteres"),
+    check('descripcion').isLength({min: 10, max: 300}).withMessage("Debe contener de 10 a 300 caracteres"),
     check('id_productoCat').custom((value, { req }) => {
         if (value === -1 || value === '') {
             throw new Error('Selecciona una categoría válida');
         }
         return true;
     }),
-    check('desc2').isLength({min: 20, max: 80}).withMessage("Debe contener de 20 a 80 caracteres"),    
+    check('desc2').isLength({min: 20, max: 500}).withMessage("Debe contener de 20 a 300 caracteres"),    
     check('imagen').custom((value, { req }) => {
         if (req.fileError) {
             throw new Error('Adjunte una imagen válida');
